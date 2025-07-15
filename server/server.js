3aedcbfa0d98fc2e142b991619ea4f87f3afc54e
Flukie <iq5823@gmail.com>
2025-07-15 23:18:40 +0700

    Add a Server.js File

--- server/server.js
+++ server/server.js
@@ -1,19 +1,27 @@
 const express = require('express');
-const app = express(); //create Express App.
+const app = express();
 const cors = require('cors');
+const getDB = require('./models/DataBase'); // make sure the file name is correct
 
 app.use(cors({
-    origin: ['http://localhost:3000'], // อนุญาตเฉพาะ origin นี้
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // อนุญาตเฉพาะ methods นี้
-    allowedHeaders: ['Content-Type', 'Authorization'] // อนุญาตเฉพาะ headers นี้
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.use(express.json({ limit: '10mb' }));  // เพิ่ม limit เป็น 10MB หรือมากกว่านั้น
+
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 
+// ดึงทั้งหมดมาจาก public-relations โดยเป็นข้อมูล ่json
+app.get('/public-relations', async (req, res) => {
+  const db = await getDB();
+  const [rows] = await db.execute('SELECT * FROM public_relations');
+  res.json(rows); 
+});
 
 
 
-
-
-app.listen(5000, () => {console.log("Server started on port 5000") })
+app.listen(5000, () => {
+    console.log("Server started on port 5000");
+});
