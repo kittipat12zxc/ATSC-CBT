27395328f384d7cea20f929c12ab91b863fa4d7e
guitar <guitartv123@gmail.com>
2025-07-16 15:21:10 +0700

    add ApplyExam.js in server

--- server/Routes/controllers/ApplyExam.js
+++ server/Routes/controllers/ApplyExam.js
@@ -1,36 +1,29 @@
-import express from 'express';
-import db from '../../models/DataBaes.js';
+const express = require('express');
+const db = require('../../models/DataBase.js'); // Import getDB function
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('ApplyExam API is working!');
-});
-
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const {
-    prefix, firstName, lastName, birthday, thai_id,
+    prefix ,firstName, lastName, birthday, thai_id,
     phone_number, email, province, district, Subdistrict,
     zipcode, Additional
-  } = req.body;
+  } = req.body || '';
 
   const sql = `
     INSERT INTO exam_enrollments 
     (prefix, firstName, lastName, birthday, thai_id, phone_number, email, province, district, Subdistrict, zipcode, Additional) 
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
   `;
-
-  db.query(sql, [
-    prefix, firstName, lastName, birthday, thai_id,
+  try {
+    const [result] = await db.query(sql, [prefix ,firstName, lastName, birthday, thai_id,
     phone_number, email, province, district, Subdistrict,
-    zipcode, Additional
-  ], (err, result) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ message: 'เกิดข้อผิดพลาด' });
-    }
+    zipcode, Additional]);
     return res.json({ message: 'บันทึกข้อมูลสำเร็จ!' });
-  });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: 'เกิดข้อผิดพลาด' });
+  }
 });
 
-export default router;
+module.exports = router;
