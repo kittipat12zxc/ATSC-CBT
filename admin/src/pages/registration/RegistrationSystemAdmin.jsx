266ec88fa425ff7948a1a09bc45432c3a883a5f7
Flukie <iq5823@gmail.com>
2025-08-05 23:11:24 +0700

    Change Color

--- admin/src/pages/registration/RegistrationSystemAdmin.jsx
+++ admin/src/pages/registration/RegistrationSystemAdmin.jsx
@@ -33,9 +33,19 @@ function RegistrationSystemAdmin() {
         setData(res.data);
       })
       .catch((error) => {
-        console.error("Error fetching data from RegistrationSystem backend", error);
+        console.log("Error fetching data from RegistrationSystem backend", error);
       });
   }, []);
+
+
+  // กำหนดสีใน Array ทั้งหมด 4 อัน ไล่เฉดสี from สี to สี
+  const gradients = [  
+ "from-cyan-500 via-teal-400 to-gray-200",  
+"from-pink-500 via-pink-400 to-pink-200",  
+"from-green-500 via-green-300 to-green-100",   
+"from-rose-500 via-rose-400 to-rose-100",   
+];
+
 
   return (
     <div className='max-w-[97%] mx-auto mt-[30px] w-full font-sans text-gray-800'>
@@ -48,18 +58,19 @@ function RegistrationSystemAdmin() {
       {/* เอาไว้โชว์ รหัสการสอบ เเละ ชื่อชุดข้อสอบ */}
       <main className='max-w-[95%] mx-auto mt-[80px] w-full'>
         <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          {data.map((exam) => (
+          {data.map((exam,index) => ( // วนลูป data เอา exam ที่เป็นค่าข้อมูล เเละ index เป็นตัวเลข Array ไว้ใช้ในการกำหนดตำเเหน่งสีในตัวเเปร gradients
             <Link to={`/TestRegis/${exam.examination_id}`} key={exam.examination_id}>
-            <div className="bg-gradient-to-r from-[#0a183d] to-[#1e3a8a] text-white rounded-xl shadow-lg p-5 flex items-center space-x-4">
+            <div className={`bg-gradient-to-r ${gradients[index]} text-white rounded-xl shadow-lg p-5 flex items-center space-x-4`}>
               <div className="flex-shrink-0">
                 <UserIcon className="h-12 w-12 text-white/90" />
               </div>
               <div>
-                 <p className="text-xl font-bold">
-                  รหัสการสอบ : {exam.examination_id}
-                </p>
-                <p className="text-sm text-white/80">
+               
+                <p className="text-xl font-bold ">
                   ชื่อชุดข้อสอบ : {exam.exam_set_name}
+                </p>
+                  <p className="text-sm text-white/80">
+                  รหัสการสอบ : {exam.examination_id}
                 </p>
               </div>
             </div>
